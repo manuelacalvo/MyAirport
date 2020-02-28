7a9ed62671033c7dc508a6edd7f6b0b6c5af112c
Nightglowie <sophie.pages8@orange.fr>
2020-02-28 11:39:20 +0100

    configuration d'un dbcontextoption

--- MyAirport.EF/MyAirportContext .cs
+++ MyAirport.EF/MyAirportContext .cs
@@ -3,23 +3,27 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Text;
+using Microsoft.Extensions.Logging;
 
 namespace MCSP.MyAirport.EF
 {
     public class MyAirportContext : DbContext
     {
-        public MyAirportContext()
+        public MyAirportContext(DbContextOptions<MyAirportContext> options) : base(options)
         {
 
         }
+
+        //public static readonly ILoggerFactory MyAirportLoggerFactory = LoggerFactory.Create(builder => { builder.AddConsole(); }); 
+
         public DbSet<Bagage> Bagages { get; set; }
         public DbSet<Vol> Vols { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        /*protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseSqlServer(ConfigurationManager.ConnectionStrings["MyAirportDatabase"].ConnectionString);
-          
-        }
+            options.UseLoggerFactory(MyAirportLoggerFactory);
+        }*/
     }
 
 }
