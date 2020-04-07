bb639787ed2bd0692de91684db2e19d30ec713a3
manuelacalvo <manuela.calvo@edu.ece.fr>
2020-04-07 11:30:36 +0200

    contextFactory

---
+++ MyAirport.EF/MyAirportContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using MCSP.MyAirport.EF;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace MCSP.MyAiport.EF
+{
+    class MyAirportContextFactory : IDesignTimeDbContextFactory<MyAirportContext>
+    {
+        public MyAirportContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MyAirportContext>(); optionsBuilder.UseSqlServer(@"Data Source=localhost,1433;Initial Catalog=MyAirport;User ID=croisillon;Password=Croisillon4!;"); var loggerFactory = LoggerFactory.Create(builder => {
+                builder.AddFilter("Microsoft", LogLevel.Debug).AddFilter("System", LogLevel.Warning);
+            });
+            optionsBuilder.UseLoggerFactory(loggerFactory);
+            return new MyAirportContext(optionsBuilder.Options);
+        }
+    }
+}
