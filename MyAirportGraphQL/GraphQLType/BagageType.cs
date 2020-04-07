c9e7207b1b664b0bd2fc6584f2fc56e6753da371
manuelacalvo <manuela.calvo@edu.ece.fr>
2020-04-07 15:47:55 +0200

    GraphQL

---
+++ MyAirportGraphQL/GraphQLType/BagageType.cs
@@ -0,0 +1,28 @@
+﻿using GraphQL.Types;
+using MCSP.MyAirport.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAirportGraphQL.GraphQLType
+{
+    public class BagageType: ObjectGraphType<Bagage>
+    {
+        public BagageType()
+        {
+            Field(x => x.BagageId);
+            Field(x => x.Classe);
+            Field(x => x.CodeIata);
+            Field(x => x.DateCreation);
+            Field(x => x.Destination);
+            Field(x => x.Escale);
+            Field(x => x.Prioritaire);
+            Field(x => x.Ssur);
+            Field(x => x.Sta);
+
+
+        }
+      
+    }
+}
---
+++ MyAirportGraphQL/Query.cs
@@ -0,0 +1,19 @@
+﻿using GraphQL.Types;
+using MCSP.MyAirport.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAirportGraphQL
+{
+    public class AirportQuery: ObjectGraphType
+    {
+        public AirportQuery(MyAirportContext db)
+        {
+            Field<ListGraphType<AirportQuery>>(
+                "bagages",
+                resolve: context => db.Bagages.ToList());
+        }
+    }
+}
