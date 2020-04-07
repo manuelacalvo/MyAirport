66e47e1b2295ad4dabcd06f5353d16dc15fdb415
manuelacalvo <manuela.calvo@edu.ece.fr>
2020-04-07 12:38:51 +0200

    Bagage et Vol type

--- MyAirportGraphSQL/GraphQLType/BagageType.cs
+++ MyAirportGraphSQL/GraphQLType/BagageType.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using GraphQL.Types;
+using MCSP.MyAirport.EF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MyAirportGraphQL.GraphQLType
+namespace MyAirportGraphSQL.GraphQLType
 {
-    public class BagageType
+    public class BagageType: ObjectGraphType<Bagage>
     {
+        public BagageType()
+        {
+            Field(x => x.BagageId);
+            Field(x => x.Classe);
+            Field(x => x.CodeIata);
+            Field(x => x.DateCreation);
+            Field(x => x.Destination);
+            Field(x => x.Escale);
+            Field(x => x.Prioritaire);
+            Field(x => x.Ssur);
+            Field(x => x.Sta);
+
+
+        }
+      
     }
 }
--- MyAirportGraphSQL/GraphQLType/VolType.cs
+++ MyAirportGraphSQL/GraphQLType/VolType.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using GraphQL.Types;
+using MCSP.MyAirport.EF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MyAirportGraphQL.GraphQLType
+namespace MyAirportGraphSQL.GraphQLType
 {
-    public class VolType
+    public class VolType : ObjectGraphType<Vol>
     {
+        public VolType()
+        {
+            Field(x => x.Cie);
+            Field(x => x.Des);
+            Field(x => x.Dhc);
+            Field(x => x.Imm);
+            Field(x => x.Lig);
+            Field(x => x.Pax);
+            Field(x => x.Pkg);
+            Field(x => x.VolId);
+            
+
+
+        }
+
     }
 }
