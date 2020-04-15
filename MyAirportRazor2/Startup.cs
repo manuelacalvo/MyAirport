f4543dcf65ea8cb76abe4f08f05cb2d74d73933b
Nightglowie <sophie.pages8@orange.fr>
2020-04-15 10:37:50 +0200

    addDbContext

--- MyAirportRazor2/Startup.cs
+++ MyAirportRazor2/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MCSP.MyAirport.EF;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,9 @@ namespace MCSP.MyAirport.Razor
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddDbContext<MyAirportContext>(option =>
+            option.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MyAirport;Integrated Security=True"));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
