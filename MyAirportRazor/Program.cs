8ee1cc6f98f368a2e6b1005e3d4318b3f4b8d6ff
manuelacalvo <manuela.calvo@edu.ece.fr>
2020-04-15 10:09:23 +0200

    null

--- MyAirportRazor/Program.cs
+++ MyAirportRazor/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace MyAirportRazor
+namespace MCSP.MyAirport.Razor
 {
     public class Program
     {
--- MyAirportRazor/Startup.cs
+++ MyAirportRazor/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace MyAirportRazor
+namespace MCSP.MyAirport.Razor
 {
     public class Startup
     {
