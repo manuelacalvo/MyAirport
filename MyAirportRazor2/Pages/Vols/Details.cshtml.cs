59a041feb246fbcc95ed64a03ba74a1b58ff5427
Nightglowie <sophie.pages8@orange.fr>
2020-04-15 10:12:38 +0200

    CRUD bagage and vol OK

---
+++ MyAirportRazor2/Pages/Vols/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MCSP.MyAirport.EF;
+
+namespace MCSP.MyAirport.Razor
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly MCSP.MyAirport.EF.MyAirportContext _context;
+
+        public DetailsModel(MCSP.MyAirport.EF.MyAirportContext context)
+        {
+            _context = context;
+        }
+
+        public Vol Vol { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Vol = await _context.Vols.FirstOrDefaultAsync(m => m.VolId == id);
+
+            if (Vol == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ MyAirportRazor2/Pages/Vols/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MCSP.MyAirport.EF;
+
+namespace MCSP.MyAirport.Razor
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MCSP.MyAirport.EF.MyAirportContext _context;
+
+        public IndexModel(MCSP.MyAirport.EF.MyAirportContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Vol> Vol { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Vol = await _context.Vols.ToListAsync();
+        }
+    }
+}
